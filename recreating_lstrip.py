7bf3bc9d60e6ea5b9da80ffddd301700531955a1
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 00:33:54 +0800

    pseudocode

---
+++ recreating_lstrip.py
@@ -0,0 +1,8 @@
+# get string_input
+
+# iterate over the indexes in string input
+    # check if the indexed character is not a space
+        # slice string input at the current index if true
+        # end loop
+
+# print string_input
