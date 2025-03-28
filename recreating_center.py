b90fc3623217336ae621d4d1f58d135f549350c5
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 16:20:15 +0800

    justify if spaces can be even in both sides and print

--- recreating_center.py
+++ recreating_center.py
@@ -8,19 +8,21 @@ string_input = input("Enter string to be centered: ")
 if total_characters <= len(string_input):
     # pass
     pass
+
 # else if the spaces to be filled can be divided evenly by left and right
 elif (total_characters - len(string_input)) % 2 == 0:
     # assign spaces as total_characters - length of string_input
     spaces = total_characters - len(string_input)
-    print(spaces)
     # concatenate half the spaces in the left and right
+    string_input = " " * (spaces // 2) + string_input + " " * (spaces // 2)
+
 # else if the spaces to be filled are not even in left and right
 else:
     # assign spaces as total_characters - length of string_input
     spaces = total_characters - len(string_input)
-    print(spaces)
     # assign left_spaces as rounded down spaces divided by 2
     # assign right_spaces as spaces - left spaces
     # concatenate the left_spaces and right_spaces to string_input
 
 # print string_input
+print(f'RESULT: "{string_input}"')
