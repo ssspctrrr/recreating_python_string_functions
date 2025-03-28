f7a78f3ffd7e79636aadfb7d16cb5fbf49f800b3
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 12:07:44 +0800

    get string input

--- recreating_lstrip.py
+++ recreating_lstrip.py
@@ -1,4 +1,6 @@
 # get string_input
+string_input = input("Enter string (preferably with several spaces at the start): ")
+print(string_input)
 
 # iterate over the indexes in string input
     # check if the indexed character is not a space
