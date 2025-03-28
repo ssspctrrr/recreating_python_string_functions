81932ae3f1aa2248d57b61406451f57e37850893
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 13:53:52 +0800

    concatenate characters to result

--- recreating_lower.py
+++ recreating_lower.py
@@ -1,6 +1,5 @@
 # get string_input
 string_input = input("Enter string to be lowercased: ")
-print(string_input)
 
 # define result variable
 result = ""
@@ -10,12 +9,13 @@ for index in range(len(string_input)):
 
     # check if character is uppercased
     if string_input[index].isupper():
-        print(string_input[index])
         # get ascii code of character
         # concatenate the lowercase to result
+        result += string_input[index]
     # else if character is not uppercased:
     else:
-        print(string_input)
         # concatenate current character to result
+        result += string_input[index]
 
 # print result
+print(result)
