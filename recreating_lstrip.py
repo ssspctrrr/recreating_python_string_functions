95fd55a07e52f9d35f12534db66e03f507ca71c0
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 12:29:54 +0800

    iterate over indexes

--- recreating_lstrip.py
+++ recreating_lstrip.py
@@ -1,10 +1,11 @@
 # get string_input
 string_input = input("Enter string (preferably with several spaces at the start): ")
-print(string_input)
 
-# iterate over the indexes in string input
+# iterate over the indexes in_string input
+for index in range(0, len(string_input)):
+    print(index)
     # check if the indexed character is not a space
-        # slice string input at the current index if true
+        # slice string_input at the current index if true
         # end loop
 
 # print string_input
