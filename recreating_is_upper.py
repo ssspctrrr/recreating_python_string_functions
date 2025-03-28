f6a7fb5bd41b27710e80c3124dc2b6bf810094ea
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 14:51:06 +0800

    pseudocode

---
+++ recreating_is_upper.py
@@ -0,0 +1,18 @@
+# get string_input
+
+# define is_uppercase as False
+
+# iterate over the characters of string_input
+    # get ascii_code of character
+    # check if current character is not alphabetical
+        # continue to next iteration
+    # else if ascii_code is in range of 65 to 90 and character is alphabetical
+        # set is_uppercase as True
+    # else if ascii_code is not in range of 65 to 90 and character is alphabetical
+        # set is_uppercase as False
+        # break loop
+# else:
+    # if is_uppercase is True
+        # print "string is uppercased"
+    # else
+        # print "string is not uppercased"
