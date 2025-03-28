ab44bbaab4c96663b810945a533371f09c2c7770
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 16:33:02 +0800

    get string input and iterate over its characters

--- recreating_swapcase.py
+++ recreating_swapcase.py
@@ -1,6 +1,9 @@
 # get string_input
+string_input = input("Enter string to reverse its casing: ")
 
 # iterate over the characters in string_input
+for character in string_input:
+    print(character)
     # check if the current character is lowercased
         # convert current character to its uppercase
     # check if current character is uppercased
