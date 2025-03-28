7d006fb7320c1aeedd8c595ce8812149b26c2185
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 16:43:14 +0800

    improve pseudocode logic, for loop, and if-else

--- recreating_swapcase.py
+++ recreating_swapcase.py
@@ -1,15 +1,23 @@
 # get string_input
 string_input = input("Enter string to reverse its casing: ")
 
+# define result variable
+
 # iterate over the characters in string_input
-for character in string_input:
+for index in range(len(string_input)):
     # check if the current character is lowercased
-    if character.islower():
-        print(character, "lower")
-        # convert current character to its uppercase
+    if string_input[index].islower():
+        # assign swap_character as the uppercase of current character
+        print(string_input[index], "lower")
+        # concatenate swap_character to result
     # check if current character is uppercased
-    elif character.isupper():
-        print(character, "upper")
-        # convert current character to its lowercase
+    elif string_input[index].isupper():
+        # assign swap_character as the lowercase of current character
+        print(string_input[index], "upper")
+        # concatenate swap_character to result
+    # else
+    else:
+        print(string_input[index])
+        # concatenate current character to result
 
 # print string_input
