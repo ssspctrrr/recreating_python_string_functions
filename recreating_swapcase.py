6a81c6b7117fc3c6236ee75273addbbb5a345905
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 16:31:23 +0800

    pseudocode

---
+++ recreating_swapcase.py
@@ -0,0 +1,9 @@
+# get string_input
+
+# iterate over the characters in string_input
+    # check if the current character is lowercased
+        # convert current character to its uppercase
+    # check if current character is uppercased
+        # convert current character to its lowercase
+
+# print string_input
