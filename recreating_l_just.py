40d98dd6f840ddbff5c57c211cf5f446cf58d836
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 15:35:11 +0800

    pseudocode

---
+++ recreating_l_just.py
@@ -0,0 +1,9 @@
+# get number of total_characters
+
+# get string_input
+
+# check if total_characters is greater than string_input
+    # get the number of spaces to fill
+    # concatenate the spaces to string_input
+
+# print string_input
