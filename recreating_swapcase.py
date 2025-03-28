8f7ff88cd5831617df43961010351436f66b4541
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 16:35:22 +0800

    check if character is lower or upper

--- recreating_swapcase.py
+++ recreating_swapcase.py
@@ -3,10 +3,13 @@ string_input = input("Enter string to reverse its casing: ")
 
 # iterate over the characters in string_input
 for character in string_input:
-    print(character)
     # check if the current character is lowercased
+    if character.islower():
+        print(character, "lower")
         # convert current character to its uppercase
     # check if current character is uppercased
+    elif character.isupper():
+        print(character, "upper")
         # convert current character to its lowercase
 
 # print string_input
