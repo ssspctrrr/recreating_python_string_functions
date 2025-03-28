fa1715f0c43cc474efed480747728e4c9e861143
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 16:46:12 +0800

    define results and assign swap_character

--- recreating_swapcase.py
+++ recreating_swapcase.py
@@ -2,18 +2,21 @@
 string_input = input("Enter string to reverse its casing: ")
 
 # define result variable
+result = ""
 
 # iterate over the characters in string_input
 for index in range(len(string_input)):
     # check if the current character is lowercased
     if string_input[index].islower():
         # assign swap_character as the uppercase of current character
-        print(string_input[index], "lower")
+        swap_character = string_input[index].upper()
+        print(swap_character)
         # concatenate swap_character to result
     # check if current character is uppercased
     elif string_input[index].isupper():
         # assign swap_character as the lowercase of current character
-        print(string_input[index], "upper")
+        swap_character = string_input[index].lower()
+        print(swap_character)
         # concatenate swap_character to result
     # else
     else:
