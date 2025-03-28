2b0f840a20c00181dbde7cf2f2f4edeba98e11b3
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 17:08:15 +0800

    pseudocode

---
+++ recreating_title.py
@@ -0,0 +1,11 @@
+# get string_input
+
+# define result
+
+# assign words by splitting string_input using " "
+
+# iterate over the word in words
+    # capitalize word
+    # concatenate result and word
+
+# print result
