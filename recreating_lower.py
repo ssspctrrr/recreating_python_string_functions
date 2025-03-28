c8a5df5f750275e3ac270d2e93f1e4db5d1a6465
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 13:50:13 +0800

    improved pseudocode logic, define result, else clause

--- recreating_lower.py
+++ recreating_lower.py
@@ -2,6 +2,9 @@
 string_input = input("Enter string to be lowercased: ")
 print(string_input)
 
+# define result variable
+result = ""
+
 # iterate over the characters of string_input
 for index in range(len(string_input)):
 
@@ -9,6 +12,10 @@ for index in range(len(string_input)):
     if string_input[index].isupper():
         print(string_input[index])
         # get ascii code of character
-        # replace current character to its lowercase
+        # concatenate the lowercase to result
+    # else if character is not uppercased:
+    else:
+        print(string_input)
+        # concatenate current character to result
 
-# print string_input
+# print result
