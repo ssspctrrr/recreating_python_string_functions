0b829d53ef400a244a21159bf89d055d9ca5845b
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 17:27:06 +0800

    iterate over indexes

--- recreating_title.py
+++ recreating_title.py
@@ -5,9 +5,11 @@ string_input = input("Enter string to put in title-casing: ")
 result = ""
 
 # iterate over the indexes of the characters in string_input
+for index in range(len(string_input)):
+    print(index, string_input[index])
     # check if current index == 0 or previous character == " "
         # concatenate result and uppercase of current character
     # else
         # concatenate the lowercase of current character
-        
+
 # print result
