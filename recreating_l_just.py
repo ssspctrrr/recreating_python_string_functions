988ef12a113d1441cba08481473bac9eafa6ea66
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 15:37:52 +0800

    get total_characters and string_input

--- recreating_l_just.py
+++ recreating_l_just.py
@@ -1,6 +1,8 @@
 # get number of total_characters
+total_characters = int(input("Enter the number of total characters: "))
 
 # get string_input
+string_input = input("Enter string to be left-justified: ")
 
 # check if total_characters is greater than string_input
     # get the number of spaces to fill
