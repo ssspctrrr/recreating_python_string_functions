2c5cafc93cb50aeed2c1f2df92720c61a1743e5d
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 15:42:17 +0800

    check if space needs added and get number of spaces

--- recreating_l_just.py
+++ recreating_l_just.py
@@ -5,7 +5,10 @@ total_characters = int(input("Enter the number of total characters: "))
 string_input = input("Enter string to be left-justified: ")
 
 # check if total_characters is greater than string_input
+if total_characters > len(string_input):
     # get the number of spaces to fill
+    spaces = total_characters - len(string_input)
+    print(spaces)
     # concatenate the spaces to string_input
 
 # print string_input
