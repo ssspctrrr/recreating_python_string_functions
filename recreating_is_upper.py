54223a98638ffe347269da6c3acc63bc6001525c
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 15:06:12 +0800

    check if character is lower or uppercased

--- recreating_is_upper.py
+++ recreating_is_upper.py
@@ -13,13 +13,18 @@ for index in range(len(string_input)):
     if string_input[index].isalpha() == False:
         # continue to next iteration
         continue
-    else:
-        print(string_input[index])
     # else if ascii_code is in range of 65 to 90 and character is alphabetical
+    elif ascii_code in range(65, 91):
         # set is_uppercase as True
+        is_uppercase = True
     # else if ascii_code is not in range of 65 to 90 and character is alphabetical
+    else:
         # set is_uppercase as False
+        is_uppercase = False
         # break loop
+
+    print(string_input[index], is_uppercase)
+
 # else:
 else:
     print("loop else triggered")
