3845cee360b74833e0fe3756cc01bba1c14db283
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 17:02:51 +0800

    lowercase the rest and print

--- recreating_capitalize.py
+++ recreating_capitalize.py
@@ -3,9 +3,11 @@ string_input = input("Enter string to be capitalized: ")
 
 # assign result as uppercase of the first character of string_input
 result = string_input[0].upper()
-print(result)
 
-# check if length of string_input is greater than 1 
+# check if length of string_input is greater than 1
+if len(string_input) > 1:
     # concatenate result and lowercase of the 2nd to the ending characters of string_input
+    result += string_input[1:].lower()
 
 # print result
+print(result)
