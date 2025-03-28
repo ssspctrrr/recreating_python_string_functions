cfd9e722fe075132717c3d9f03380898a6df895e
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 16:56:59 +0800

    pseudocode

---
+++ recreating_capitalize.py
@@ -0,0 +1,8 @@
+# get string_input
+
+# assign result as uppercase of the first character of string_input
+
+# check if length of string_input is greater than 1 
+    # concatenate result and lowercase of the 2nd to the ending characters of string_input
+
+# print result
