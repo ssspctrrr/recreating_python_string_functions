9333dde92f5438f9f9506ca80a83aed963676c79
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 12:51:22 +0800

    remove left spaces and print result

--- recreating_lstrip.py
+++ recreating_lstrip.py
@@ -5,8 +5,10 @@ string_input = input("Enter string (preferably with several spaces at the start)
 for index in range(0, len(string_input)):
     # check if the indexed character is not a space
     if string_input[index] != " ":
-        print(string_input[index])
         # slice string_input at the current index if true
+        string_input = string_input[index:]
         # end loop
+        break
 
-# print string_input
+# print result
+print(string_input)
