1bb5a08dfa5c261c20b4673c72c8ef06345f629b
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 15:26:08 +0800

    check and print if suffix is at the end of string_input

--- recreating_ends_with.py
+++ recreating_ends_with.py
@@ -5,6 +5,10 @@ string_input = input("Enter string to be checked: ")
 suffix = input("Enter suffix to be located at the end of string: ")
 
 # check if suffix is at the end of string_input
+if string_input[-len(suffix):] == suffix:
     # print True
+    print(f"TRUE. {string_input} ends with {suffix}.")
 # else
+else:
     # print False
+    print(f"FALSE. {string_input} does not end with {suffix}.")
