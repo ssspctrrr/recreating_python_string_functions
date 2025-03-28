1fd7cfbd8df788d6671b649cdfa165c1775a4f0a
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 12:57:16 +0800

    get string_input and prefix

--- recreating_remove_prefix.py
+++ recreating_remove_prefix.py
@@ -1,6 +1,11 @@
 # get string_input
+string_input = input("Enter string: ")
 
 # get prefix
+prefix = input("Enter prefix to be removed: ")
+
+print(string_input)
+print(prefix)
 
 # check if prefix is at the start of string_input
     # slice string input if true
