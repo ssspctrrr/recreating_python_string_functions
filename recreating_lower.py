f841ab03b7279c599897b904c98928543f9288c9
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 13:22:49 +0800

    pseudocode

---
+++ recreating_lower.py
@@ -0,0 +1,8 @@
+# get string_input
+
+# iterate over the characters of string_input
+    # check if character is uppercased
+        # get ascii code of character
+        # replace current character to its lowercase
+
+# print string_input
