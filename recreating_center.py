fe0d1ccbc48279e585ad1f19751d605d98acb4d1
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 16:03:29 +0800

    pseudocode

---
+++ recreating_center.py
@@ -0,0 +1,16 @@
+# get total_characters
+
+# get string_input
+
+# check if total_characters is greater than length of string_input
+    # pass
+# else if the spaces to be filled can be divided evenly by left and right
+    # assign spaces as total_characters - length of string_input
+    # concatenate half the spaces in the left and right
+# else if the spaces to be filled are not even in left and right
+    # assign spaces as total_characters - length of string_input
+    # assign left_spaces as rounded down spaces divided by 2
+    # assign right_spaces as spaces - left spaces
+    # concatenate the left_spaces and right_spaces to string_input
+
+# print string_input
