c681954352fdf600911b126881079281f4ffc217
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 12:43:36 +0800

    checks if character is space

--- recreating_lstrip.py
+++ recreating_lstrip.py
@@ -3,8 +3,9 @@ string_input = input("Enter string (preferably with several spaces at the start)
 
 # iterate over the indexes in_string input
 for index in range(0, len(string_input)):
-    print(index)
     # check if the indexed character is not a space
+    if string_input[index] != " ":
+        print(string_input[index])
         # slice string_input at the current index if true
         # end loop
 
