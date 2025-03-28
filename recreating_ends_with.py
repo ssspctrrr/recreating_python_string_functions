8d5acfe660ea288fbdfb31e9377f10bd18868925
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 15:18:04 +0800

    get string input and suffix

--- recreating_ends_with.py
+++ recreating_ends_with.py
@@ -1,6 +1,8 @@
 # get string_input
+string_input = input("Enter string to be checked: ")
 
 # get suffix
+suffix = input("Enter suffix to be located at the end of string: ")
 
 # check if suffix is at the end of string_input
     # print True
