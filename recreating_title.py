17f9b2950307d5c39d78f5d653320467b2a7b469
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 17:30:42 +0800

    check if current character must be upper or lowercased

--- recreating_title.py
+++ recreating_title.py
@@ -6,10 +6,13 @@ result = ""
 
 # iterate over the indexes of the characters in string_input
 for index in range(len(string_input)):
-    print(index, string_input[index])
     # check if current index == 0 or previous character == " "
+    if index == 0 or string_input[index - 1] == " ":
+        print(string_input[index], "to be capitalized")
         # concatenate result and uppercase of current character
     # else
+    else:
+        print(string_input[index])
         # concatenate the lowercase of current character
 
 # print result
