23c7b4ad222005b5ebb23ca9298b75090536fd79
Leo Caldosa <caldosaleo@gmail.com>
2025-03-28 13:29:31 +0800

    iterate over characters and check if upper

--- recreating_lower.py
+++ recreating_lower.py
@@ -3,7 +3,11 @@ string_input = input("Enter string to be lowercased: ")
 print(string_input)
 
 # iterate over the characters of string_input
+for index in range(len(string_input)):
+
     # check if character is uppercased
+    if string_input[index].isupper():
+        print(string_input[index])
         # get ascii code of character
         # replace current character to its lowercase
 
