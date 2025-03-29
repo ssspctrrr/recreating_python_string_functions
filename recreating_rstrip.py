9d7a551d7c8231026c06007f00daeff81f08dfe3
Leo Caldosa <caldosaleo@gmail.com>
2025-03-29 17:42:43 +0800

    pseudocode

---
+++ recreating_rstrip.py
@@ -0,0 +1,9 @@
+# get string_input
+
+# check if length of string_input is more than 1
+    # iterate over the reverse indexing of string_input
+        # check if current character is not " "
+            # split string_input from start to current character
+            # break loop
+
+# print string_input
