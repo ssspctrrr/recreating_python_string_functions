932e6c392d4b1e4dd562a1f54b0444de323dfa2f
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 23:32:06 +0800

    pseudocode

---
+++ recreating_index.py
@@ -0,0 +1,10 @@
+# get string_input
+
+# get string_to_find
+
+# iterate over index of string_input in lengths of string_to_find
+    # if current character/s == string_to_find
+        # print current index
+        # break loop
+# else
+    # raise ValueError with message "substring not found"
