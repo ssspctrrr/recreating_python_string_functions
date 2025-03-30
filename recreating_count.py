6fef5a47f03caca4ffb9c01224b5bc510b5c9dfb
Leo Caldosa <caldosaleo@gmail.com>
2025-03-31 00:49:59 +0800

    pseudocode

---
+++ recreating_count.py
@@ -0,0 +1,17 @@
+# get string_input
+
+# get string_to_count
+
+# define index variable as 0
+
+# define count_of_string as 0
+
+# iterate while index < length of string_input:
+    # check if string_input[index:length of string_to_find] == string_to_find
+        # increment count_of_string by 1
+        # increment index by length of string_to_find
+        # continue
+    
+    # increment index by 1
+
+# print count_of_string
