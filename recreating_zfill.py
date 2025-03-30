a4dbed1f8d513315b2edfe00331a1a8c9bb43562
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 21:38:46 +0800

    pseudocode

---
+++ recreating_zfill.py
@@ -0,0 +1,14 @@
+# get number of total_characters
+
+# get string_input
+
+# check if length of string_input is greater than or equal to total_characters
+    # pass
+# else if first character is "+" or "-"
+    # assign zeroes as total_characters - length of string_input[1:]
+    # concatenate the sign, zeroes, to string_input
+# else
+    # assign zeroes as total_character - length of string_input
+    # concatenate zeros to string_input
+
+# print string_input
