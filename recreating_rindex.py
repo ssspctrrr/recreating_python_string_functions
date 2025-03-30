03026b902f112e078c37af58496211bc55792ac3
Leo Caldosa <caldosaleo@gmail.com>
2025-03-31 00:12:11 +0800

    print index of first occurence of string_to_find

--- recreating_rindex.py
+++ recreating_rindex.py
@@ -6,11 +6,10 @@ string_to_find = input("Enter character/s to find from the end: ")
 
 # iterate over reverese indexing of string_input in lengths of string_to_find
 for index in range(len(string_input) - 1, -1, -1):
-    print(index, string_input[index:index + len(string_to_find)])
     # check if current character/s == string_to_find
     if string_input[index:index + len(string_to_find)] == string_to_find:
         # print index
-        print("string_to_find found")
+        print(index)
         # break loop
         break
 # else
