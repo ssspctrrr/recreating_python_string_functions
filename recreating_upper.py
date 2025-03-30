cd6c7fb7309fa05069a05fcf69e004b50548e399
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 17:27:10 +0800

    get string_input and define result

--- recreating_upper.py
+++ recreating_upper.py
@@ -1,6 +1,11 @@
 # get string_input
+string_input = input("Enter string to be uppercased: ")
 
 # define result as empty string
+result = ""
+
+print(string_input)
+print(result)
 
 # iterate over the characters of string_input
     # check if current character is lowercased
