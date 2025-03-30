881a9cfa2a5631a67f9e33145fe064c23b7919e8
Leo Caldosa <caldosaleo@gmail.com>
2025-03-31 00:55:04 +0800

    get string_input and string_to_count, define index and count_of_string

--- recreating_count.py
+++ recreating_count.py
@@ -1,10 +1,14 @@
 # get string_input
+string_input = input("Enter string to be checked: ")
 
 # get string_to_count
+string_to_count = input("Enter string to count: ")
 
 # define index variable as 0
+index = 0
 
 # define count_of_string as 0
+count_of_string = 0
 
 # iterate while index < length of string_input:
     # check if string_input[index:length of string_to_find] == string_to_find
