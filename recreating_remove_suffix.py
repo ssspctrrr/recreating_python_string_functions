10395b4d81d5d155ed53cf8349212380d75961c0
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 17:12:10 +0800

    check if suffix is in end of string

--- recreating_remove_suffix.py
+++ recreating_remove_suffix.py
@@ -4,10 +4,9 @@ string_input = input("Enter string to remove suffix: ")
 # get suffix
 suffix = input("Enter suffix to remove: ")
 
-print(string_input)
-print(suffix)
-
 # check if suffix == end of string_input
+if suffix == string_input[-len(suffix):]:
     # slice string if true
+    print(True)
 
 # print string_input
