53c6dc90f537bc560b05276304a77764a9285eac
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 20:54:55 +0800

    pseudocode

---
+++ recreating_r_just.py
@@ -0,0 +1,9 @@
+# get total_characters
+
+# get string_input
+
+# check if total_characters geater than length of string_input
+    # assign spaces as total_character - length of string_input
+    # concatenate spaces at start of string_input
+
+# print string_input
