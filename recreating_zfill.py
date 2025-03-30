5c4e27b12b6693d3cfc5246d8fadfc3c6e1d98ae
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 21:47:10 +0800

    check if zeroes need added and print string

--- recreating_zfill.py
+++ recreating_zfill.py
@@ -4,11 +4,13 @@ total_characters = int(input("Enter number of characters: "))
 # get string_input
 string_input = input("Enter string to be added with zeroes: ")
 
-print(total_characters, string_input)
-
 # check if length of string_input is greater than or equal to total_characters
+if len(string_input) >= total_characters:
     # pass
+    pass
 # else if first character is "+" or "-"
+else:
+    print("add zeroes")
     # assign zeroes as total_characters - length of string_input[1:]
     # concatenate the sign, zeroes, to string_input
 # else
@@ -16,3 +18,4 @@ print(total_characters, string_input)
     # concatenate zeros to string_input
 
 # print string_input
+print(string_input)
