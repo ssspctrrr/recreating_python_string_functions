f83df654b103620d9617ed88731979a5a4ab2776
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 20:42:18 +0800

    print if string starts with prefix or not

--- recreating_starts_with.py
+++ recreating_starts_with.py
@@ -7,6 +7,8 @@ prefix = input("Enter prefix to find and check: ")
 # check if prefix is start of string_input
 if prefix == string_input[:len(prefix)]:
     # print "True. string_input starts with prefix."
-    print("prefix found")
+    print(f'TRUE. "{string_input}" starts with "{prefix}".')
 # else
+else:
     # print "False. string_input does not start with prefix"
+    print(f'FALSE. "{string_input} starts with "{prefix}".')
