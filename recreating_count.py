f2a4c96d4a61681db3860caecd1f48a16813e083
Leo Caldosa <caldosaleo@gmail.com>
2025-03-31 01:05:13 +0800

    corrected pseudocode error

--- recreating_count.py
+++ recreating_count.py
@@ -13,7 +13,7 @@ count_of_string = 0
 # iterate while index < length of string_input:
 while index < len(string_input):
     print(index, string_input[index:index + len(string_to_count)])
-    # check if string_input[index:length of string_to_find] == string_to_find
+    # check if string_input[index:index + length of string_to_count] == string_to_count
         # increment count_of_string by 1
         # increment index by length of string_to_find
         # continue
