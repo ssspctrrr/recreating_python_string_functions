3cfbea760fe142d2be97075c7cc91d2b8d5d7a87
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 20:25:24 +0800

    pseudocode

---
+++ recreating_starts_with.py
@@ -0,0 +1,8 @@
+# get string_input
+
+# get prefix
+
+# check if prefix is start of string_input
+    # print "True. string_input starts with prefix."
+# else
+    # print "False. string_input does not start with prefix"
