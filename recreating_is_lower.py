5957334232aa5e43d1deb25d8f0bc04a012ce70f
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 20:08:07 +0800

    check if character not alphabetical

--- recreating_is_lower.py
+++ recreating_is_lower.py
@@ -8,10 +8,13 @@ is_lower = False
 for character in string_input:
     # get ascii_code of current character
     ascii_code = ord(character)
-    print(ascii_code, character)
 
-    # check if current character is alphabetical
+    # check if current character is not alphabetical
+    if character.isalpha() == False:
         # continue to next iteration
+        continue
+    else:
+        print(ascii_code, character)
     # else if ascii_code is in range of 97 to 122
         # set is_lower as True
     # else
