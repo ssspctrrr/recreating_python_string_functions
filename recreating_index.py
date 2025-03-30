23a94f9cf82a432341b1a804a9801e072b4a9102
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 23:38:27 +0800

    for-else to iterate over index of string in lengths of string_to_find

--- recreating_index.py
+++ recreating_index.py
@@ -4,12 +4,13 @@ string_input = input("Enter string to be checked: ")
 # get string_to_find
 string_to_find = input("Enter character/s to find: ")
 
-print(string_input)
-print(string_to_find)
-
 # iterate over index of string_input in lengths of string_to_find
+for index in range(0, len(string_input)):
+    print(index, string_input[index:index + len(string_to_find)])
     # if current character/s == string_to_find
         # print current index
         # break loop
 # else
+else:
+    print("loop else triggered")
     # raise ValueError with message "substring not found"
