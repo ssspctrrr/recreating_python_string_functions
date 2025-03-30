bc1ad3ed2af1ac66425e4f8007ba0312c9d89444
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 17:25:09 +0800

    pseudocode

---
+++ recreating_upper.py
@@ -0,0 +1,12 @@
+# get string_input
+
+# define result as empty string
+
+# iterate over the characters of string_input
+    # check if current character is lowercased
+        # get ascii code of current character
+        # concatenate its uppercase to result
+    # else
+        # concatenate current character to result
+
+# print result
