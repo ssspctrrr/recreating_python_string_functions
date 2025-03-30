5144cb3ef14c74d126382b0799d8121e818a5f51
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 17:37:49 +0800

    check if character should be uppercased

--- recreating_upper.py
+++ recreating_upper.py
@@ -6,11 +6,14 @@ result = ""
 
 # iterate over the characters of string_input
 for character in string_input:
-    print(character)
     # check if current character is lowercased
+    if character.islower():
+        print(character, "convert to uppercase")
         # get ascii code of current character
         # concatenate its uppercase to result
     # else
+    else:
+        print(character, "stay")
         # concatenate current character to result
 
 # print result
