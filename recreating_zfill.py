502a3963e0036dff1c6d6bb2c6c6fe6714935792
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 22:50:39 +0800

    add zeroes if there is no sign

--- recreating_zfill.py
+++ recreating_zfill.py
@@ -12,14 +12,14 @@ if len(string_input) >= total_characters:
 elif string_input[0] in ["+", "-"]:
     # assign zeroes as total_characters - length of string_input
     zeroes = total_characters - len(string_input)
-    print(zeroes)
     # concatenate the sign, zeroes, to string_input
     string_input = string_input[0] + "0" * zeroes + string_input[1:]
 # else
 else:
-    print("no signs")
     # assign zeroes as total_character - length of string_input
+    zeroes = total_characters - len(string_input)
     # concatenate zeros to string_input
+    string_input = "0" * zeroes + string_input
 
 # print string_input
 print(string_input)
