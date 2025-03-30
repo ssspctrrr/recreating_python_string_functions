49059eb71e3be0d1e21c133294154e9e6d09d442
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 17:14:35 +0800

    remove suffix and print

--- recreating_remove_suffix.py
+++ recreating_remove_suffix.py
@@ -7,6 +7,7 @@ suffix = input("Enter suffix to remove: ")
 # check if suffix == end of string_input
 if suffix == string_input[-len(suffix):]:
     # slice string if true
-    print(True)
+    string_input = string_input[:-len(suffix)]
 
 # print string_input
+print(string_input)
