1ae8b207c9300dc8d2d75bccbf683b937776d14e
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 20:59:27 +0800

    get total_characters and string_input

--- recreating_r_just.py
+++ recreating_r_just.py
@@ -1,6 +1,10 @@
 # get total_characters
+total_characters = int(input("Enter number of total characters: "))
 
 # get string_input
+string_input = input("Enter string to be right-justified: ")
+
+print(total_characters, string_input)
 
 # check if total_characters geater than length of string_input
     # assign spaces as total_character - length of string_input
