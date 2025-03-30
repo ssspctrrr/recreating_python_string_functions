7c14e9fa5c4e3f335a3f92c6839036cb92b5a9f7
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 21:03:23 +0800

    check if space needed and get number of spaces

--- recreating_r_just.py
+++ recreating_r_just.py
@@ -4,10 +4,11 @@ total_characters = int(input("Enter number of total characters: "))
 # get string_input
 string_input = input("Enter string to be right-justified: ")
 
-print(total_characters, string_input)
-
 # check if total_characters geater than length of string_input
+if total_characters > len(string_input):
     # assign spaces as total_character - length of string_input
+    spaces = total_characters - len(string_input)
+    print(spaces)
     # concatenate spaces at start of string_input
 
 # print string_input
