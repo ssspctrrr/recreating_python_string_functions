f0e789a94411c958aa19f245201405f1b553827c
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 17:36:33 +0800

    iterate over characters

--- recreating_upper.py
+++ recreating_upper.py
@@ -4,10 +4,9 @@ string_input = input("Enter string to be uppercased: ")
 # define result as empty string
 result = ""
 
-print(string_input)
-print(result)
-
 # iterate over the characters of string_input
+for character in string_input:
+    print(character)
     # check if current character is lowercased
         # get ascii code of current character
         # concatenate its uppercase to result
