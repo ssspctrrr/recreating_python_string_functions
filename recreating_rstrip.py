216641eb8dd3caafcba180bb727ddd1dd43262a4
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 15:17:05 +0800

    check when to split string

--- recreating_rstrip.py
+++ recreating_rstrip.py
@@ -7,8 +7,11 @@ if len(string_input) > 1:
     for index in range(len(string_input) - 1, -1, -1):
         print(index, string_input[index])
         # check if current character is not " "
+        if string_input[index] != " ":
             # split string_input from start to current character
             # break loop
+            print("split here")
+            break
 
 # print string_input
-print(string_input)
+print(f'RESULT: "{string_input}"')
