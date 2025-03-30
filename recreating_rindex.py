9a12138ac7338f8aa26bad3eb0d3f99ebdad1b5e
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 23:55:12 +0800

    pseudocode

---
+++ recreating_rindex.py
@@ -0,0 +1,10 @@
+# get string_input
+
+# get string_to_find
+
+# iterate over reverese indexing of string_input in lengths of string_to_find
+    # check if current character/s == string_to_find
+        # print index
+        # break loop
+# else
+    # raise ValueError with message "substring not found"
