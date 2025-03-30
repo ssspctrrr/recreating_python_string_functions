79703599448fd696951545acb0787492e77576b8
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 18:07:59 +0800

    pseudocode

---
+++ recreating_is_lower.py
@@ -0,0 +1,19 @@
+# get string_input
+
+# define is_lower as False
+
+# iterate over the characters of string_input
+    # get ascii_code of current character
+
+    # check if current character is alphabetical
+        # continue to next iteration
+    # else if ascii_code is in range of 97 to 122
+        # set is_lower as True
+    # else
+        # set is_lower as False
+        # break loop
+
+# check if is_lower == True
+    # print "String is lowercased"
+# else
+    # print "String is not lowercased"
