e6c639faf7724421af04a7ba704f0e87adb4d09b
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 23:34:10 +0800

    get string input and string to find

--- recreating_index.py
+++ recreating_index.py
@@ -1,6 +1,11 @@
 # get string_input
+string_input = input("Enter string to be checked: ")
 
 # get string_to_find
+string_to_find = input("Enter character/s to find: ")
+
+print(string_input)
+print(string_to_find)
 
 # iterate over index of string_input in lengths of string_to_find
     # if current character/s == string_to_find
