af486e112cd198c7779d82d6fb275f4b819c09e0
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 23:43:44 +0800

    print index of first occurence of string_to_find

--- recreating_index.py
+++ recreating_index.py
@@ -6,11 +6,10 @@ string_to_find = input("Enter character/s to find: ")
 
 # iterate over index of string_input in lengths of string_to_find
 for index in range(0, len(string_input)):
-    print(index, string_input[index:index + len(string_to_find)])
     # if current character/s == string_to_find
     if string_input[index:index + len(string_to_find)] == string_to_find:
-        print("string found")
         # print current index
+        print(index)
         # break loop
         break
 # else
