8db9f87abf14ddecbd43ffd72fd564af932189a4
Leo Caldosa <caldosaleo@gmail.com>
2025-03-30 23:57:05 +0800

    get string_input and string_to_find

--- recreating_rindex.py
+++ recreating_rindex.py
@@ -1,6 +1,10 @@
 # get string_input
+string_input = input("Enter string to be checked: ")
 
 # get string_to_find
+string_to_find = input("Enter character/s to find from the end: ")
+
+print(string_input, string_to_find)
 
 # iterate over reverese indexing of string_input in lengths of string_to_find
     # check if current character/s == string_to_find
